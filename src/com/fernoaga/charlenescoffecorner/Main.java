1575c1b683f3f126e29af3f7166facccfbcfc9e3
Vlad Fernoaga <fernoaga.vlad@gmail.com>
2021-08-27 13:55:08 +0300

    Create simple print function when running from main()

--- src/com/fernoaga/charlenescoffecorner/Main.java
+++ src/com/fernoaga/charlenescoffecorner/Main.java
@@ -1,8 +1,37 @@
 package com.fernoaga.charlenescoffecorner;
 
+import com.fernoaga.charlenescoffecorner.model.OrderRequest;
+import com.fernoaga.charlenescoffecorner.model.Receipt;
+import com.fernoaga.charlenescoffecorner.repository.mock.CoffeeShopProductRepositoryMockImpl;
+import com.fernoaga.charlenescoffecorner.service.CoffeeCornerService;
+import com.fernoaga.charlenescoffecorner.service.CoffeeCornerServiceImpl;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        CoffeeCornerService coffeeCornerService =
+                new CoffeeCornerServiceImpl(new CoffeeShopProductRepositoryMockImpl());
+
+
+        List<OrderRequest> request = Arrays.asList(new OrderRequest(CoffeeShopProductRepositoryMockImpl.COFFEE_SMALL,
+                        Optional.of((Arrays.asList(CoffeeShopProductRepositoryMockImpl.TOPPING_SPECIAL_ROAST_COFFEE,
+                                CoffeeShopProductRepositoryMockImpl.TOPPING_FOAMED_MILK)))),
+                new OrderRequest(CoffeeShopProductRepositoryMockImpl.BACON_ROLL, Optional.empty())
+        );
+        Receipt receipt = coffeeCornerService.orderProducts(request, Optional.empty());
+        printReceipt(receipt);
+    }
+
+    private static void printReceipt(Receipt receipt) {
+
+        for (var r : receipt.getEntries()) {
+            System.out.println(r.getProductName() + "       " + r.getPrice());
+        }
+        System.out.println("\n");
+        System.out.println("Total price:       " + receipt.getTotalPrice());
     }
 }
